7d2ab291ed7fe5220b02988fe1c0750ea7878a13
rohan-dv <rohan8dv@gmail.com>
2026-05-26 04:32:13 +0000

    Add screen clear and keyboard input

--- boot/boot.asm
+++ boot/boot.asm
@@ -2,22 +2,34 @@
 [ORG 0x7C00]
 
 start:
+    ; Set video mode (clears screen)
+    mov ah, 0x00
+    mov al, 0x03
+    int 0x10
+
+    ; Print message
     mov si, message
 
 print_loop:
-    lodsb               ; load next character into AL
+    lodsb
     cmp al, 0
-    je halt
+    je wait_for_key
 
-    mov ah, 0x0E        ; BIOS teletype function
-    int 0x10            ; print character
+    mov ah, 0x0E
+    mov bh, 0x00
+    mov bl, 0x0A
+    int 0x10
 
     jmp print_loop
+
+wait_for_key:
+    mov ah, 0x00
+    int 0x16
 
 halt:
     jmp $
 
-message db "Hello from Bootloader!", 0
+message db "Hello from Bootloader! Press any key...", 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
