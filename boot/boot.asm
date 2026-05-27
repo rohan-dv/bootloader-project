cf0cbd44d6678109eabcf79cd1bf5dc380e8d6ba
rohan-dv <rohan8dv@gmail.com>
2026-05-27 05:25:09 +0000

    initial bootloader with keyboard input

--- boot/boot.asm
+++ boot/boot.asm
@@ -2,34 +2,60 @@
 [ORG 0x7C00]
 
 start:
-    ; Set video mode (clears screen)
+    ; Clear screen (80x25 text mode)
     mov ah, 0x00
     mov al, 0x03
     int 0x10
 
-    ; Print message
+    ; Print first message
     mov si, message
+    call print_string
 
-print_loop:
-    lodsb
-    cmp al, 0
-    je wait_for_key
-
-    mov ah, 0x0E
-    mov bh, 0x00
-    mov bl, 0x0A
-    int 0x10
-
-    jmp print_loop
-
-wait_for_key:
+    ; Wait for a key press
     mov ah, 0x00
     int 0x16
+
+    ; Save pressed key
+    mov bl, al
+
+    ; Move to next line
+    mov ah, 0x0E
+    mov al, 0x0D
+    int 0x10
+
+    mov al, 0x0A
+    int 0x10
+
+    ; Print second message
+    mov si, pressed_msg
+    call print_string
+
+    ; Print the pressed key
+    mov ah, 0x0E
+    mov al, bl
+    int 0x10
 
 halt:
     jmp $
 
+; -------------------------
+; Print string routine
+; -------------------------
+print_string:
+.next_char:
+    lodsb
+    cmp al, 0
+    je .done
+
+    mov ah, 0x0E
+    int 0x10
+    jmp .next_char
+
+.done:
+    ret
+
 message db "Hello from Bootloader! Press any key...", 0
+pressed_msg db "You pressed: ", 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
