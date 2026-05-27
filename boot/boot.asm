66943df0f94d05a1dc35f446726b8b9a0f27a73b
rohan-dv <rohan8dv@gmail.com>
2026-05-27 14:08:46 +0000

    Added boot sector that prints continuous keyboard input

--- boot/boot.asm
+++ boot/boot.asm
@@ -8,37 +8,27 @@ start:
 print_loop:
     lodsb
     cmp al, 0
-    je wait_for_key
+    je keyboard_loop
 
     mov ah, 0x0E
     int 0x10
     jmp print_loop
 
-wait_for_key:
+
+keyboard_loop:
     ; Wait for key press
     mov ah, 0x00
     int 0x16
 
-    ; Save pressed key (ASCII in AL)
-    mov bl, al
-
-    ; New line
-    mov ah, 0x0E
-
-    mov al, 0x0D
-    int 0x10
-
-    mov al, 0x0A
-    int 0x10
-
     ; Print pressed key
-    mov al, bl
+    mov ah, 0x0E
     int 0x10
 
-halt:
-    jmp halt
+    ; Keep listening forever
+    jmp keyboard_loop
 
-message db "Welcome to RohanOS! Press any key:", 0
+
+message db "Welcome to RohanOS! Type something: ", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
