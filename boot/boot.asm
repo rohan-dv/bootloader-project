2e572975fd9958f9784718b87cb28b415f79c510
rohan-dv <rohan8dv@gmail.com>
2026-05-27 14:12:55 +0000

    added terminal like keyboard handling

--- boot/boot.asm
+++ boot/boot.asm
@@ -20,15 +20,53 @@ keyboard_loop:
     mov ah, 0x00
     int 0x16
 
-    ; Print pressed key
+    ; Check Enter key (ASCII 13)
+    cmp al, 13
+    je handle_enter
+
+    ; Check Backspace key (ASCII 8)
+    cmp al, 8
+    je handle_backspace
+
+    ; Normal character
     mov ah, 0x0E
     int 0x10
-
-    ; Keep listening forever
     jmp keyboard_loop
 
 
-message db "Welcome to RohanOS! Type something: ", 0
+handle_enter:
+    mov ah, 0x0E
+
+    ; carriage return
+    mov al, 0x0D
+    int 0x10
+
+    ; line feed
+    mov al, 0x0A
+    int 0x10
+
+    jmp keyboard_loop
+
+
+handle_backspace:
+    mov ah, 0x0E
+
+    ; Move cursor back
+    mov al, 8
+    int 0x10
+
+    ; Print space (erase char)
+    mov al, ' '
+    int 0x10
+
+    ; Move back again
+    mov al, 8
+    int 0x10
+
+    jmp keyboard_loop
+
+
+message db "Welcome to RohanOS! Type something:", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
