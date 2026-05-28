2a8ccdc34e25acfe17a10ff0a5f0e0c2b103bdd7
rohan-dv <rohan8dv@gmail.com>
2026-05-28 17:44:47 +0000

    command parser and interactive boot monitor completed

--- boot/boot.asm
+++ boot/boot.asm
@@ -33,6 +33,7 @@ read_input:
 
     jmp read_input
 
+
 backspace_pressed:
     cmp di, input_buffer
     je read_input
@@ -51,6 +52,7 @@ backspace_pressed:
 
     jmp read_input
 
+
 enter_pressed:
     mov byte [di], 0
 
@@ -61,7 +63,81 @@ enter_pressed:
     mov al, 10
     int 0x10
 
+    call process_command
     jmp main_loop
+
+
+process_command:
+    ; check help
+    mov si, input_buffer
+    mov di, help_cmd
+    call compare_strings
+    cmp ax, 1
+    je help_found
+
+    ; check clear
+    mov si, input_buffer
+    mov di, clear_cmd
+    call compare_strings
+    cmp ax, 1
+    je clear_found
+
+    ; check reboot
+    mov si, input_buffer
+    mov di, reboot_cmd
+    call compare_strings
+    cmp ax, 1
+    je reboot_found
+
+    jmp unknown
+
+
+help_found:
+    mov si, help_message
+    call print_string
+    ret
+
+
+clear_found:
+    mov ax, 0x0003
+    int 0x10
+    ret
+
+
+reboot_found:
+    int 0x19
+    ret
+
+
+unknown:
+    mov si, unknown_message
+    call print_string
+    ret
+
+
+compare_strings:
+.compare_loop:
+    mov al, [si]
+    mov bl, [di]
+
+    cmp al, bl
+    jne .not_equal
+
+    cmp al, 0
+    je .equal
+
+    inc si
+    inc di
+    jmp .compare_loop
+
+.equal:
+    mov ax, 1
+    ret
+
+.not_equal:
+    mov ax, 0
+    ret
+
 
 print_string:
     mov ah, 0x0E
@@ -77,8 +153,16 @@ print_string:
 .done:
     ret
 
+
 welcome_msg db "Bootloader started!",13,10,0
 prompt db "> ",0
+
+help_cmd db "help",0
+clear_cmd db "clear",0
+reboot_cmd db "reboot",0
+
+help_message db "Commands: help clear reboot",13,10,0
+unknown_message db "Unknown command",13,10,0
 
 input_buffer times 64 db 0
 
